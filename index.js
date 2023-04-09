950c4dc5e283a680bece6fbc3ecbf97214d2ed6f
Gaus Al Munir Tushar <tushar01910533370@gmail.com>
2023-04-10 04:33:14 +0600

    setup in linux and starting practice

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
-var fullName = 'jack';
-fullName = 'john';
+var userName = 'jack';
+console.log(userName);
+// add two nubmer
+function addNumber(num1, num2) {
+    console.log(num1 + num2);
+}
+addNumber(29, 29);
--- index.ts
+++ index.ts
@@ -1,2 +1,9 @@
-let fullName : string = 'jack'
-fullName = 'john'
+let userName: string = 'jack';
+console.log(userName)
+
+
+// add two nubmer
+function addNumber (num1:number, num2:number) {
+    console.log(num1 + num2)
+}
+addNumber(29,29)
